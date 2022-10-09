045c78dccd8bcae435901190ca3b08b98c2b122e
EvgenyWas <evgeniywas@gmail.com>
2022-10-09 12:00:18 +0100

    feature: add scroll behavior to page top by change a route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/home',
