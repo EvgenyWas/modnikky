51fe24e02c36c13ef957d75997e4ab91bc98c653
EvgenyWas <evgeniywas@gmail.com>
2022-11-11 10:01:25 +0000

    feature: add pictures for shop section in webp format and update template of shop section component

--- src/components/Home/ShopSection.vue
+++ src/components/Home/ShopSection.vue
@@ -11,24 +11,34 @@
             </div>
             <div class="shop-section__images">
                 <div class="shop-section__image-box">
-                    <img class="shop-section__image" data-src="/src/assets/shop/instagram-image-placeholder-1.jpg"
-                        alt="Outfit image">
+                    <picture class="shop-section__image">
+                        <source srcset="/src/assets/shop/instagram-image-placeholder-1.webp" type="image/webp">
+                        <img data-src="/src/assets/shop/instagram-image-placeholder-1.jpg" alt="Outfit image">
+                    </picture>
                 </div>
                 <div class="shop-section__image-box">
-                    <img class="shop-section__image" data-src="/src/assets/shop/instagram-image-placeholder-2.jpg"
-                        alt="Outfit image">
+                    <picture class="shop-section__image">
+                        <source srcset="/src/assets/shop/instagram-image-placeholder-2.webp" type="image/webp">
+                        <img data-src="/src/assets/shop/instagram-image-placeholder-2.jpg" alt="Outfit image">
+                    </picture>
                 </div>
                 <div class="shop-section__image-box">
-                    <img class="shop-section__image" data-src="/src/assets/shop/instagram-image-placeholder-3.jpg"
-                        alt="Outfit image">
+                    <picture class="shop-section__image">
+                        <source srcset="/src/assets/shop/instagram-image-placeholder-3.webp" type="image/webp">
+                        <img data-src="/src/assets/shop/instagram-image-placeholder-3.jpg" alt="Outfit image">
+                    </picture>
                 </div>
                 <div class="shop-section__image-box">
-                    <img class="shop-section__image" data-src="/src/assets/shop/instagram-image-placeholder-4.jpg"
-                        alt="Outfit image">
+                    <picture class="shop-section__image">
+                        <source srcset="/src/assets/shop/instagram-image-placeholder-4.webp" type="image/webp">
+                        <img data-src="/src/assets/shop/instagram-image-placeholder-4.jpg" alt="Outfit image">
+                    </picture>
                 </div>
                 <div class="shop-section__image-box">
-                    <img class="shop-section__image" data-src="/src/assets/shop/instagram-image-placeholder-5.jpg"
-                        alt="Outfit image">
+                    <picture class="shop-section__image">
+                        <source srcset="/src/assets/shop/instagram-image-placeholder-5.webp" type="image/webp">
+                        <img data-src="/src/assets/shop/instagram-image-placeholder-5.jpg" alt="Outfit image">
+                    </picture>
                 </div>
             </div>
             <h5 class="shop-section__name">
